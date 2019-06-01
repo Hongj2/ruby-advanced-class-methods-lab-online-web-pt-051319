4330aeb23f46f9487f2cb7e02f82fef7c967aaa3
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-01 18:38:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,6 +54,13 @@ def self.new_from_filename (filename)
 
 end
   
+def self.create_from_filename(filename)
+  song = self.new_from_filename(filename)
+  @@all << song
+end
 
+def self.destroy_all
+  self.all.clear
+end
  
  end
